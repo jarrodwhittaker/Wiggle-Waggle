06c9a270f2de7fe5a21b54e39e458857f131f30b
SAE\kporteous <1005781@student.sae.edu.au>
2017-11-14 10:54:49 +1000

    Fixed node spawning

--- Assets/NodeManager.cs
+++ Assets/NodeManager.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class NodeManager : MonoBehaviour {
 
     public float distance;
 
     public GameObject nodePrefab;
-    public GameObject currentNode;
     public GameObject lastNode;
 
     public Mesh nodeMesh;
@@ -15,48 +15,49 @@ public class NodeManager : MonoBehaviour {
     Vector3 mousePos;
     Vector3 currentMousePos;
 
-	// Use this for initialization
-	void Start ()
+    public Transform startPoint;
+
+    public Renderer rend;
+
+    void Awake()
     {
 
-
-
-        nodePrefab = GameObject.FindGameObjectWithTag("Node");
-
-        mousePos = Input.mousePosition;
-
-        Vector3 distance = Vector3.Scale(transform.localScale, nodeMesh.bounds.size);
-
-
-        currentNode = gameObject;
-        lastNode = currentNode;
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        rend = nodePrefab.GetComponent<Renderer>();
+        Instantiate(nodePrefab, new Vector3(startPoint.transform.position.x, startPoint.transform.position.y, startPoint.transform.position.z), Quaternion.identity);
 
     }
 
     // Update is called once per frame
     void Update ()
     {
-        currentNode = this.gameObject;
 
-        currentMousePos = Input.mousePosition;
-
-
-
-        if (/*mousePos.x > currentMousePos.x + 2 || mousePos.x < currentMousePos.x - 2*/ Input.GetKeyDown(KeyCode.Alpha1))
+        if (nodePrefab == null)
         {
-
-            Vector3 SpawnPoint = currentNode.transform.position - lastNode.transform.position;
-            SpawnPoint.Normalize();
-            SpawnPoint *= distance;
-
-            currentNode = Instantiate(nodePrefab, SpawnPoint, Quaternion.identity);
-
-            lastNode.GetComponent<HingeJoint>().connectedBody = currentNode.GetComponent<Rigidbody>();
-
-            lastNode = currentNode;
         }
 
-	}
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Spawned");
+            Instantiate(nodePrefab, new Vector3(lastNode.transform.position.x, lastNode.transform.position.y - rend.bounds.size.y, lastNode.transform.position.z), Quaternion.identity);
+        }
 
+        NodeCount();
+    }
 
+    void NodeCount()
+    {
+        List<GameObject> nodes = GameObject.FindGameObjectsWithTag("Node").ToList();
+
+        foreach (GameObject node in nodes)
+        {
+            
+            int numOfNodes = nodes.Count;
+            lastNode = nodes[numOfNodes - 1];
+        }
+
+    }
 }
