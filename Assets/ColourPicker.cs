83dcc90fdc90cc701ecc87388b32255215d28279
SAE\kporteous <1005781@student.sae.edu>
2017-12-13 20:19:29 +1000

    Added colout picking for the players

---
+++ Assets/ColourPicker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourPicker : MonoBehaviour {
+
+    Colour myMaterial;
+    public LineRenderer rend;
+
+    void Awake()
+    {
+        rend = this.gameObject.GetComponent<LineRenderer>();
+        myMaterial = GetComponent<Colour>();
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        rend.material = myMaterial.colour;
+	}
+}
