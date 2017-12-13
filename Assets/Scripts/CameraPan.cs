36f847aa0cce6d08352234a347a1f31c9e7b798b
SAE\kporteous <1005781@student.sae.edu>
2017-12-13 19:23:54 +1000

    Camera pan is working. Added wobble effect to the camera

--- Assets/Scripts/CameraPan.cs
+++ Assets/Scripts/CameraPan.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraPan : MonoBehaviour {
+public class CameraPan : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    VerletPhysics player1Script;
+    VerletPhysics player2Script;
+
+    // Use this for initialization
+    void Start()
+    {
+        player1 = GameObject.FindGameObjectWithTag("Player1");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
+        player1Script = player1.GetComponent<VerletPhysics>();
+        player2Script = player2.GetComponent<VerletPhysics>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMidPoint();
+    }
+
+    void FollowMidPoint()
+    {
+
+        Vector3 midPlayers = (player1.transform.position - player2.transform.position) * 0.5f + player2.transform.position;
+        Vector3 midNodes = (player1Script.nodes[player1Script.nodes.Count - 1].currentPosition - player2Script.nodes[player2Script.nodes.Count - 1].currentPosition) *0.5f + player2Script.nodes[player2Script.nodes.Count - 1].currentPosition;
+        Vector3 midPoint = (midPlayers - midNodes) * 0.5f + midNodes;
+
+        Debug.Log(midPlayers + " | " + midNodes + " | " + midPoint);
+        Camera.main.transform.position = new Vector3(midPoint.x, midPoint.y, -player1Script.nodes.Count - 3);
+    }
 }
